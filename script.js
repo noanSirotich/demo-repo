5bae196927755be02f1fe1f2612a3a5e040a4062
noanSirotich <noan.sirotich@gmail.com>
2023-06-22 15:25:51 +0200

    Added logic

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // this is my main js file
+for (let broj = i; i <= 20; i++) {
+    if (broj % 2 === 0) {
+        console.log("Broj je paran");
+    } else {
+        console.log("Broj je neparan");
+    }
+}
